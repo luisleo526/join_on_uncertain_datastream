5c4f568d97b299dd551df63d0aeaf79008dbcaae
HaoLiangWen <luisleo52655@gmail.com>
2023-12-27 15:28:38 +0800

    change png

--- test_prune_power.py
+++ test_prune_power.py
@@ -1,8 +1,8 @@
 from itertools import combinations
-from typing import List
 
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.metrics import classification_report
 from tqdm import tqdm
 
 from uncertain_object import UncertainObject
@@ -10,40 +10,113 @@ from utils import generator
 
 if __name__ == '__main__':
 
-    objects: List[UncertainObject] = []
+    num_objects = int(input('Number of objects: '))
+    threshold = float(input('Threshold: '))
 
-    dims = int(input("Enter the number of dimensions: "))
-    num_objects = int(input("Enter the number of objects: "))
-    epsilon = float(input("Enter the value of epsilon: "))
     total_tests = int(num_objects * (num_objects - 1) / 2)
-    for _ in range(num_objects):
-        mean = np.random.randint(100, 150, 1)
-        std = np.random.randint(1, 11, 1)
-        num_samples = np.random.randint(30, 100, 1)
-        objects.append(UncertainObject(
-            generator(num_samples, [], [],
-                      [(d, mean - (d + 1) * std, mean + (d + 1) * std) for d in range(dims)]))
-        )
+    dims = np.array(list(range(1, 8)), dtype=int)
+    dims = np.power(2, dims)
 
-    prune_power = np.zeros((3, 10))
-    betas = np.linspace(0, 0.5, 10)
-    for i, j in tqdm(combinations(objects, 2), total=total_tests):
-        for k in range(10):
-            prune_power[0, k] += i.ej(j, epsilon, beta=betas[k])
-            prune_power[1, k] += i.iej(j, epsilon, beta=betas[k])
-            prune_power[2, k] += i.o_iej(j, epsilon, beta=betas[k])
+    results = np.zeros((3, dims.size, total_tests), dtype=int)
 
-    prune_power /= total_tests * 100
-    prune_power = 1.0 - prune_power
-    prune_power *= 100
+    for dim_idx in range(dims.size):
 
-    plt.figure()
-    plt.plot(betas, prune_power[0, :], label='EJ', marker='o')
-    plt.plot(betas, prune_power[1, :], label='IEJ', marker='o')
-    plt.plot(betas, prune_power[2, :], label='O_IEJ', marker='o')
-    plt.legend()
-    plt.xlabel('Beta')
-    plt.ylabel('Prune power (%)')
+        dim = dims[dim_idx]
+
+        objects = []
+
+        means = np.random.uniform(-10, 10, (num_objects, dim))
+        stds = np.random.uniform(1, 5, (num_objects, dim))
+
+        # compute distance matrix of means
+        distance_matrix = np.linalg.norm(means[:, None, :] - means[None, :, :], axis=-1)
+
+        epsilon = np.quantile(distance_matrix.flatten(), threshold)
+
+        for i in range(num_objects):
+            mean = means[i]
+            std = stds[i]
+            num_samples = np.random.randint(10, 30, 1)
+
+            tri = []
+            gau = []
+            uni = []
+            for d in range(dim):
+                if d % 3 == 0:
+                    tri.append((d, mean[d] - std[d], mean[d] + std[d]))
+                elif d % 3 == 1:
+                    gau.append((d, mean[d], std[d]))
+                else:
+                    uni.append((d, mean[d] - std[d], mean[d] + std[d]))
+
+            objects.append(UncertainObject(generator(num_samples, tri, gau, uni)))
+
+        for idx, (i, j) in enumerate(tqdm(combinations(objects, 2), total=total_tests)):
+            results[0, dim_idx, idx] = i.ej(j, epsilon)
+            results[1, dim_idx, idx] = i.iej(j, epsilon)
+            results[2, dim_idx, idx] = i.o_iej(j, epsilon)
+
+    precision = np.zeros((4, dims.size))
+    recall = np.zeros((4, dims.size))
+    f1_score = np.zeros((4, dims.size))
+
+    for k in range(dims.size):
+        iej = classification_report(results[0, k], results[1, k], output_dict=True, zero_division=0.0)
+        oiej = classification_report(results[0, k], results[2, k], output_dict=True, zero_division=0.0)
+
+        precision[0, k] = iej['0']['precision']
+        recall[0, k] = iej['0']['recall']
+        f1_score[0, k] = iej['0']['f1-score']
+
+        precision[1, k] = oiej['0']['precision']
+        recall[1, k] = oiej['0']['recall']
+        f1_score[1, k] = oiej['0']['f1-score']
+
+        precision[2, k] = iej['1']['precision']
+        recall[2, k] = iej['1']['recall']
+        f1_score[2, k] = iej['1']['f1-score']
+
+        precision[3, k] = oiej['1']['precision']
+        recall[3, k] = oiej['1']['recall']
+        f1_score[3, k] = oiej['1']['f1-score']
+
+    fig, axs = plt.subplots(3, 2, figsize=(10, 13))
+
+    axs[0][0].plot(dims, precision[0], label='IEJ', marker='o')
+    axs[0][0].plot(dims, precision[1], label='O_IEJ', marker='o')
+    axs[0][0].set_title('Precision (0)')
+    axs[0][0].set_xscale('log', base=2)
+    axs[0][0].legend()
+
+    axs[1][0].plot(dims, recall[0], label='IEJ', marker='o')
+    axs[1][0].plot(dims, recall[1], label='O_IEJ', marker='o')
+    axs[1][0].set_title('Recall (0)')
+    axs[1][0].set_xscale('log', base=2)
+    axs[1][0].legend()
+
+    axs[2][0].plot(dims, f1_score[0], label='IEJ', marker='o')
+    axs[2][0].plot(dims, f1_score[1], label='O_IEJ', marker='o')
+    axs[2][0].set_title('F1 Score (0)')
+    axs[2][0].set_xscale('log', base=2)
+    axs[2][0].legend()
+
+    axs[0][1].plot(dims, precision[2], label='IEJ', marker='o')
+    axs[0][1].plot(dims, precision[3], label='O_IEJ', marker='o')
+    axs[0][1].set_title('Precision (1)')
+    axs[0][1].set_xscale('log', base=2)
+    axs[0][1].legend()
+
+    axs[1][1].plot(dims, recall[2], label='IEJ', marker='o')
+    axs[1][1].plot(dims, recall[3], label='O_IEJ', marker='o')
+    axs[1][1].set_title('Recall (1)')
+    axs[1][1].set_xscale('log', base=2)
+    axs[1][1].legend()
+
+    axs[2][1].plot(dims, f1_score[2], label='IEJ', marker='o')
+    axs[2][1].plot(dims, f1_score[3], label='O_IEJ', marker='o')
+    axs[2][1].set_title('F1 Score (1)')
+    axs[2][1].set_xscale('log', base=2)
+    axs[2][1].legend()
 
     # save figure
-    plt.savefig(f'prune_power_{dims}-{num_objects}-{epsilon}.png', dpi=180, bbox_inches='tight')
+    plt.savefig(f'{num_objects}-{int(threshold * 100)}.png', dpi=180, bbox_inches='tight')
--- uncertain_object.py
+++ uncertain_object.py
@@ -45,27 +45,59 @@ class UncertainObject(object):
         :return: Boolean
         """
 
-        assert np.all(delta > 0.0)
+        # prob = np.zeros(self.num_dimensions)
+        #
+        # area_self = self.mbr.max - self.mbr.min + 2.0 * delta
+        # area_other = other.mbr.max - other.mbr.min + 2.0 * delta
+        #
+        # for i in range(self.num_dimensions):
+        #
+        #     if self.mbr.max[i] > other.mbr.max[i]:
+        #         if self.mbr.min[i] < other.mbr.min[i]:
+        #             area = other.mbr.max[i] - other.mbr.min[i] + 2.0 * delta[i]
+        #             prob[i] = area / area_self[i]
+        #         else:
+        #             area = other.mbr.max[i] - self.mbr.min[i] + 2.0 * delta[i]
+        #             if area < 0.0:
+        #                 area = 0.0
+        #             prob[i] = area ** 2 / (area_self[i] * area_other[i])
+        #
+        #     else:
+        #         if other.mbr.min[i] < self.mbr.min[i]:
+        #             area = self.mbr.max[i] - self.mbr.min[i] + 2.0 * delta[i]
+        #             prob[i] = area / area_other[i]
+        #         else:
+        #             area = self.mbr.max[i] - other.mbr.min[i] + 2.0 * delta[i]
+        #             if area < 0.0:
+        #                 area = 0.0
+        #             prob[i] = area ** 2 / (area_self[i] * area_other[i])
 
-        area = np.where(self.mbr.max > other.mbr.max,
-                        np.where(self.mbr.min > other.mbr.min,
-                                 other.mbr.max - self.mbr.min + 2.0 * delta,
-                                 other.mbr.max - other.mbr.min + 2.0 * delta
-                                 ),
-                        np.where(self.mbr.min > other.mbr.min,
-                                 self.mbr.max - self.mbr.min + 2.0 * delta,
-                                 self.mbr.max - other.mbr.min + 2.0 * delta
-                                 )
-                        )
+        area_self = self.mbr.max - self.mbr.min + 2.0 * delta
+        area_other = other.mbr.max - other.mbr.min + 2.0 * delta
 
-        area = np.where(area > 0.0, area, 0.0)
+        # Calculate the conditions
+        cond_self_greater = self.mbr.max > other.mbr.max
+        cond_self_smaller = self.mbr.min < other.mbr.min
 
-        prob = (np.prod(area) ** 2 / np.prod(self.mbr.max - self.mbr.min + 2.0 * delta) /
-                np.prod(other.mbr.max - other.mbr.min + 2.0 * delta))
+        # Calculate the areas based on conditions
+        area1 = np.where(cond_self_smaller,
+                         other.mbr.max - other.mbr.min + 2.0 * delta,
+                         other.mbr.max - self.mbr.min + 2.0 * delta)
+        area1 = np.maximum(area1, 0.0)  # to ensure area is not negative
 
-        assert prob <= 1.0
+        area2 = np.where(cond_self_smaller,
+                         self.mbr.max - other.mbr.min + 2.0 * delta,
+                         self.mbr.max - self.mbr.min + 2.0 * delta)
+        area2 = np.maximum(area2, 0.0)  # to ensure area is not negative
 
-        return prob > beta
+        # Calculate probabilities based on conditions
+        prob = np.where(cond_self_greater,
+                        np.where(cond_self_smaller, area1 / area_self, area1 ** 2 / (area_self * area_other)),
+                        np.where(cond_self_smaller, area2 ** 2 / (area_self * area_other), area2 / area_other))
+
+        assert np.all(prob <= 1.0)
+
+        return np.prod(prob) > beta
 
     def iej(self, other, eps: float, delta: Optional[np.ndarray] = None, beta: float = 0.0) -> bool:
         """
@@ -91,6 +123,7 @@ class UncertainObject(object):
         """
         assert self.num_dimensions == other.num_dimensions
 
-        std = self.mbr.std + other.mbr.std
+        # std = self.mbr.std + other.mbr.std
+        std = np.sqrt(np.square(self.mbr.std) + np.square(other.mbr.std))
         delta = 1.0 / (2.0 * np.linalg.norm(std, ord=1)) * eps * std
         return self._check_overlapping(other, delta, beta)
