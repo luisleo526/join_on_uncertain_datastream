f8d2f3e9d6f3c7f46925cc8cfdd2d8c99f3b4217
HaoLiangWen <luisleo52655@gmail.com>
2024-01-05 13:35:28 +0800

    debug

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import logging
+from argparse import ArgumentParser
 from pathlib import Path
 
 import numpy as np
@@ -12,18 +13,35 @@ from model import IEJModel
 
 logging.basicConfig(level=logging.INFO)
 
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('--num_objects', type=int, default=500)
+    parser.add_argument('--num_epochs', type=int, default=60)
+    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--dims', type=int, default=[1, 2, 3, 4, 5, 6, 7], nargs='+')
+    parser.add_argument('--lr', type=float, default=1e-3)
+    parser.add_argument('--gamma', type=float, default=0.9)
+    parser.add_argument('--weight_decay', type=float, default=1e-5)
+    parser.add_argument('--device', type=str, default='cuda')
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
 
-    num_objects = 500
-    num_epochs = 60
-    batch_size = 16
+    args = parse_args()
 
-    dims = np.array(list(range(1, 8)), dtype=int)
-    dims = np.power(2, dims)
+    num_objects = args.num_objects
+    num_epochs = args.num_epochs
+    batch_size = args.batch_size
+
+    device = torch.device(args.device)
 
     Path('./ckpt').mkdir(parents=True, exist_ok=True)
 
-    for dim in dims:
+    for _dim in args.dims:
+
+        dim = int(2 ** _dim)
 
         train_ds = UncertainObjectDataset(num_objects, dim, [0.05 * i for i in range(1, 11)])
         train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True, collate_fn=collate_fn, num_workers=8)
@@ -32,7 +50,7 @@ if __name__ == '__main__':
         eval_dl = DataLoader(eval_ds, batch_size=batch_size, shuffle=False, collate_fn=collate_fn, num_workers=8)
 
         model = IEJModel(dim, 4)
-        model.cuda()
+        model.to(device)
         model.train()
         optim = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
         scheduler = torch.optim.lr_scheduler.ExponentialLR(optim, gamma=0.9)
@@ -49,7 +67,7 @@ if __name__ == '__main__':
 
             model.train()
             for a, b, epsilon, min_distance in train_dl:
-                a, b, epsilon, min_distance = a.cuda(), b.cuda(), epsilon.cuda(), min_distance.cuda()
+                a, b, epsilon, min_distance = a.to(device), b.to(device), epsilon.to(device), min_distance.to(device)
                 optim.zero_grad()
                 w = model(a, b)
                 loss = iej_loss(w, a, b, epsilon, min_distance)
@@ -63,7 +81,8 @@ if __name__ == '__main__':
             with torch.no_grad():
                 acm_loss = 0.0
                 for a, b, epsilon, min_distance in eval_dl:
-                    a, b, epsilon, min_distance = a.cuda(), b.cuda(), epsilon.cuda(), min_distance.cuda()
+                    a, b, epsilon, min_distance = a.to(device), b.to(device), epsilon.to(device), min_distance.to(
+                        device)
                     w = model(a, b)
                     loss = iej_loss(w, a, b, epsilon, min_distance)
                     acm_loss += loss.item()
